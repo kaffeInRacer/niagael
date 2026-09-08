f0e9a0de12accd702cde8b36f58ea6d7267e935b
kaffeInRacer <satriaaryaxi@gmail.com>
2026-09-08 11:52:22 +0700

    update migration

--- scripts/init-databases.sql
+++ scripts/init-databases.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE product_service;
 CREATE DATABASE dynamic_pricing;
 CREATE DATABASE order_service;
+CREATE DATABASE auth_service;
--- services/auth-service/migrations/000001_users.up.sql
+++ services/auth-service/migrations/000001_users.up.sql
@@ -12,7 +12,10 @@ CREATE TABLE IF NOT EXISTS users (
 
 INSERT INTO users (id, email, password_hash, role)
 VALUES
+--   user1234
     ('10000000-0000-0000-0000-000000000001', 'tenant@demo.com', '$2a$12$J1ysIESBsfj.yptSVJ0PYeepTrHLhBUCLvPXlLBmSVzJ84xd/BoOC', 'tenant'),
+--     staff1234
     ('10000000-0000-0000-0000-000000000002', 'staff@demo.com', '$2a$12$J1ysIESBsfj.yptSVJ0PYeepTrHLhBUCLvPXlLBmSVzJ84xd/BoOC', 'staff'),
+--     admin1234
     ('10000000-0000-0000-0000-000000000003', 'admin@demo.com', '$2a$12$J1ysIESBsfj.yptSVJ0PYeepTrHLhBUCLvPXlLBmSVzJ84xd/BoOC', 'admin')
 ON CONFLICT (email) DO NOTHING;
