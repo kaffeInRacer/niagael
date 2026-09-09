3df20e5cc7d98c6d32e2996f56f0a984d51d2072
kaffeInRacer <satriaaryaxi@gmail.com>
2026-09-09 21:47:27 +0700

    fix(order): use 1-based page param with clamp=1

--- services/order-service/internal/dto/order_dto.go
+++ services/order-service/internal/dto/order_dto.go
@@ -38,5 +38,6 @@ type ListOrderParams struct {
 	OrderBy    string `form:"order_by" validate:"def_enum=created_at total_amount"`
 	OrderDir   string `form:"order_dir" validate:"def_enum=desc asc"`
 	PageSize   int32  `form:"page_size" validate:"clamp=10 100"`
-	PageOffset int32  `form:"page" validate:"clamp=0"`
+	Page       int32  `form:"page" validate:"clamp=1"`
+	PageOffset int32  `form:"-"`
 }
