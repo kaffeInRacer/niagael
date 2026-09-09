eac605c8d22beb67017056451171d4e617f9b38d
kaffeInRacer <satriaaryaxi@gmail.com>
2026-09-09 21:47:27 +0700

    fix(product): use 1-based page param with clamp=1

--- services/product/internal/dto/product_params.go
+++ services/product/internal/dto/product_params.go
@@ -12,6 +12,7 @@ type ListProductParams struct {
 	OrderBy         string  `form:"order_by" validate:"def_enum=created_at name price"`
 	OrderDir        string  `form:"order_dir" validate:"def_enum=desc asc"`
 	PageSize        int32   `form:"page_size" validate:"clamp=10 100"`
-	PageOffset      int32   `form:"page" validate:"clamp=0"`
+	Page            int32   `form:"page" validate:"clamp=1"`
+	PageOffset      int32   `form:"-"`
 	PublicOnly      bool    `form:"-"`
 }
