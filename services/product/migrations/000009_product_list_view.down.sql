9a9e83b4e22086796db0129ada3ee3bf36050257
kaffeInRacer <satriaaryaxi@gmail.com>
2026-09-11 02:29:56 +0700

    migrations(product): add product_list_view for list queries

---
+++ services/product/migrations/000009_product_list_view.down.sql
@@ -0,0 +1,4 @@
+-- =============================================
+-- Drop materialized view: product_list_view
+-- =============================================
+DROP MATERIALIZED VIEW IF EXISTS product_list_view;
